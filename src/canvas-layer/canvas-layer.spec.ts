7292501e7102a36b1fb483798da1874dd43629a3
Greg Solo <soloschenko@gmail.com>
2020-08-16 15:52:27 -0400

    Ships 1

--- src/canvas-layer/canvas-layer.spec.ts
+++ src/canvas-layer/canvas-layer.spec.ts
@@ -12,4 +12,14 @@ describe('>>> CanvasLayer', () => {
     expect(canvas1).toBe(canvas2)
     expect(Canvas).toBeCalledTimes(1)
   })
+
+  it('should create Foreground canvas only once', () => {
+    expect(Canvas).not.toBeCalled()
+
+    const canvas1 = CanvasLayer.Foreground
+    const canvas2 = CanvasLayer.Foreground
+
+    expect(canvas1).toBe(canvas2)
+    expect(Canvas).toBeCalledTimes(1)
+  })
 })
--- src/utils/canvas/canvas.ts
+++ src/utils/canvas/canvas.ts
@@ -1,4 +1,4 @@
-import { IAwake, Vector2D } from '@/utils'
+import { IAwake, Vector2D, Color } from '@/utils'
 
 export class Canvas implements IAwake {
   private _elm: HTMLCanvasElement
@@ -30,15 +30,36 @@ export class Canvas implements IAwake {
     this._ctx = ctx
   }
 
-  public FillRect(start: Vector2D, size: Vector2D, color: string): void {
+  public FillRect(start: Vector2D, size: Vector2D, color: Color): void {
     this._ctx.beginPath()
-    this._ctx.fillStyle = color
+    this._ctx.fillStyle = color.AsString()
     this._ctx.rect(start.x, start.y, size.x, size.y)
     this._ctx.fill()
   }
 
   public ClearRect(start: Vector2D, size: Vector2D): void {
     this._ctx.clearRect(start.x, start.y, size.x, size.y)
+  }
+
+  public FillCircle(center: Vector2D, radius: number, color: Color): void {
+    this._ctx.beginPath()
+    this._ctx.arc(center.x, center.y, radius, 0, Math.PI * 2)
+    this._ctx.fillStyle = color.AsString()
+    this._ctx.fill()
+  }
+
+  public SetStyle(style: Partial<CSSStyleDeclaration>): void {
+    for (const key in style) {
+      if (!Object.hasOwnProperty.call(style, key)) {
+        continue
+      }
+
+      if (!style[key]) {
+        continue
+      }
+
+      this._elm.style[key] = style[key] as string
+    }
   }
 }
 
