967acabe1fe03052378d491b4f32f1a1863ae98f
Greg Solo <gregory.soloshchenko@gmail.com>
2021-04-28 22:06:50 -0400

    movement-pathfinding-1.1

---
+++ src/utils/breadth-first-search/breadth-first-search.ts
@@ -0,0 +1,23 @@
+import { IGraph } from './breadth-first-search.h'
+import { Vector2D, Queue, Dictionary } from '@/utils'
+
+export const BreadthFirstSearch = (graph: IGraph, start: Vector2D): Dictionary<Vector2D | null> => {
+  const frontier = new Queue<Vector2D>()
+  const path: Dictionary<Vector2D | null> = {
+    [start.AsString()]: null
+  }
+
+  frontier.Enqueue(start)
+
+  while (!frontier.IsEmpty) {
+    const current = frontier.Dequeue()
+    for (const next of graph.GetNeighborsForNodeWithIndex(current)) {
+      if (!Object.prototype.hasOwnProperty.call(path, next.AsString())) {
+        frontier.Enqueue(next)
+        path[next.AsString()] = current
+      }
+    } 
+  }
+
+  return path
+}
---
+++ src/utils/queue/queue.ts
@@ -0,0 +1,24 @@
+export class Queue<T> {
+  private items: T[] = []
+
+  public get Items(): T[] {
+    return this.items
+  }
+
+  public get IsEmpty(): boolean {
+    return this.items.length === 0
+  }
+
+  public Enqueue(elm: T): void {
+    this.items.push(elm)
+  }
+
+  public Dequeue(): T {
+    const item = this.items.shift()
+    if (typeof item === 'undefined') {
+      throw new Error('Attempt to dequeue from an empty queue')
+    }
+
+    return item
+  }
+}
